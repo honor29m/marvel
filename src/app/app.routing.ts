ee856f24a1fdb004e6d2b46d227be0774bde93a7
Honi Rodriguez <honor29m@gmail.com>
2021-02-03 12:27:31 -0400

    home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-//import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { CharactersComponent } from './components/characters/characters.component';
 import { CharDetailsComponent } from './components/char-details/char-details.component';
 import { ComicsComponent } from './components/comics/comics.component';
@@ -13,7 +13,8 @@ import { ErrorComponent } from './components/error/error.component';
 
 //Array de rutas
 const appRoutes: Routes = [
-    {path: '', component: CharactersComponent},
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'characters', component: CharactersComponent},
     {path: 'characters-details/:id', component: CharDetailsComponent},
     {path: 'comics', component: ComicsComponent},
